15023ad85bc0f72c7e63c409f45ec0ebec080743
Li, Fei <34050989+lifei-cn@users.noreply.github.com>
2019-08-19 17:20:19 +0800

    Update settings.py

--- client/client/settings.py
+++ client/client/settings.py
@@ -149,7 +149,7 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
